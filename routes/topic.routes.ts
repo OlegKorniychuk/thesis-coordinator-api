50bc0fbb4433434030784249795dd45ddb497831
OlegKorniychuk <kornoleg576@gmail.com>
2025-05-02 11:53:19 +0300

    fix: add active diplomaCycle check when confirming topic

--- routes/topic.routes.ts
+++ routes/topic.routes.ts
@@ -1,8 +1,9 @@
 import {topicController} from 'controllers';
 import express from 'express';
+import {restrictToPhases} from 'middleware/restrictToPhases.middleware';
 
 const router = express.Router();
 
-router.route('/:topicId/confirm').patch(topicController.confirmTopic);
+router.route('/:topicId/confirm').patch(restrictToPhases(), topicController.confirmTopic);
 
 export default router;
