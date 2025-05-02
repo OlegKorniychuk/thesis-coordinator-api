963d7e6e1f4d40b0bf7453c5130c76b3fd8f678d
OlegKorniychuk <kornoleg576@gmail.com>
2025-05-02 11:52:02 +0300

    fix: add non empty body check when creating a bachelor

--- routes/bachelor.routes.ts
+++ routes/bachelor.routes.ts
@@ -1,12 +1,17 @@
 import {DiplomaCyclePhase} from '@prisma/client';
 import {bachelorController} from 'controllers';
 import express from 'express';
+import {checkForBody} from 'middleware/checkForBody.middleware';
 import {restrictToPhases} from 'middleware/restrictToPhases.middleware';
 
 const router = express.Router();
 
 router
   .route('/')
-  .post(restrictToPhases([DiplomaCyclePhase.preparation]), bachelorController.createBachelor);
+  .post(
+    checkForBody,
+    restrictToPhases([DiplomaCyclePhase.preparation]),
+    bachelorController.createBachelor
+  );
 
 export default router;
