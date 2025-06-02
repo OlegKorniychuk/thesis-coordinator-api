67ca546cbd7f959c89a68734d9f910587eb133e8
OlegKorniychuk <kornoleg576@gmail.com>
2025-06-02 13:03:14 +0300

    feat[admin]: add endpoint to download report

--- controllers/archivedBachelors.controller.ts
+++ controllers/archivedBachelors.controller.ts
@@ -2,6 +2,7 @@ import {ArchivedBachelor} from '@prisma/client';
 import {catchError} from '@utils/catchError';
 import {Request, Response, NextFunction} from 'express';
 import archivedBachelorService from 'services/archivedBachelor/archivedBachelor.service';
+import Excel from 'exceljs';
 
 const getYears = catchError(
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
@@ -33,4 +34,38 @@ const getByYear = catchError(
   }
 );
 
-export {getYears, getByYear};
+const generateReport = catchError(
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const year: number = parseInt(req.params.year);
+
+    const bachelorsData: ArchivedBachelor[] =
+      await archivedBachelorService.getArchivedBachelorsByYear(year);
+
+    const formattedBachelorsData: Omit<ArchivedBachelor, 'year' | 'archived_bachelor_id'>[] =
+      bachelorsData.map(({year, archived_bachelor_id, ...rest}) => rest);
+
+    const workbook = new Excel.Workbook();
+    const worksheet = workbook.addWorksheet(`Звіт ${year}`);
+
+    worksheet.columns = [
+      {header: 'ПІБ студента', key: 'full_name'},
+      {header: 'Тема дипломного проєкту', key: 'topic'},
+      {header: 'Спеціальність', key: 'specialty'},
+      {header: 'Освітня програма', key: 'academic_program'},
+      {header: 'Група', key: 'group'},
+      {header: 'ПІБ керівника', key: 'supervisor_full_name'},
+      {header: 'Вчений ступінь керівника', key: 'supervisor_degree'},
+      {header: 'Посада керівника', key: 'supervisor_position'}
+    ];
+
+    formattedBachelorsData.forEach(item => worksheet.addRow(item));
+
+    res.setHeader('Content-Type', 'application/vnd.openxmlformats');
+    res.setHeader('Content-Disposition', 'attachment; filename="report.xlsx"');
+
+    await workbook.xlsx.write(res);
+    res.end();
+  }
+);
+
+export {getYears, getByYear, generateReport};
--- routes/archivedBachelors.routes.ts
+++ routes/archivedBachelors.routes.ts
@@ -1,12 +1,19 @@
+import {UserRole} from '@prisma/client';
 import {archivedBachelorsController} from 'controllers';
 import express from 'express';
 import {protect} from 'middleware/protect.middleware';
+import {restrictToRoles} from 'middleware/restrictToRoles.middleware';
 
 const router = express.Router();
 
 router.use(protect);
 
-router.route('/years').get(archivedBachelorsController.getYears);
-router.route('/years/:year').get(archivedBachelorsController.getByYear);
+router.route('/years').get(restrictToRoles([UserRole.admin]), archivedBachelorsController.getYears);
+router
+  .route('/years/:year')
+  .get(restrictToRoles([UserRole.admin]), archivedBachelorsController.getByYear);
+router
+  .route('/years/:year/report')
+  .get(restrictToRoles([UserRole.admin]), archivedBachelorsController.generateReport);
 
 export default router;
